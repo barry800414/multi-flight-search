23e25906757d9504b01ac5655ac1a9f7dc8bebd7
barrywm <barrywm@amazon.com>
2025-09-14 13:52:25 +0800

    chore: add some comments for how to use

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import requests
 import json
 from pathlib import Path
 
+"""
+How to use it:
+1. Get test app token from https://developers.amadeus.com/
+2. My assumption is 2nd & 3rd flights' location and date are fixed, we search for pontential 1st and 4th flight location and dates
+3. After searching price, result will be stored in ./test or ./prod
+4. Use view.py to view result
+5. If you want to get real price, you need to activate prod token. 
+   (need credict card & sign contract, but there is free quota)
+"""
+
+is_test = False
+folder = "./test" if is_test else "./prod"
+
+# Get access token from https://developers.amadeus.com/
+# For production usage, you need to use credit card & sign contract to get token
+# But there is free quota.
+token = ""
+
+# Use test for testing script correctness, use prod for correct price
+endpoint = (
+    "https://test.api.amadeus.com/v2/shopping/flight-offers"
+    if is_test
+    else "https://api.amadeus.com/v2/shopping/flight-offers"
+)
+
 # SGN: 胡志明市國際機場
 # TPE: 台北桃園國際機場
 # YVR: 溫哥華國際機場
@@ -10,28 +35,19 @@ from pathlib import Path
 # CI: 中華航空
 # BR: 長榮航空
 # JX: 星宇航空
-
-is_test = False
-folder = "./test" if is_test else "./prod"
-
-token = ""
-
-endpoint = (
-    "https://test.api.amadeus.com/v2/shopping/flight-offers"
-    if is_test
-    else "https://api.amadeus.com/v2/shopping/flight-offers"
-)
-
 def search(first_location, first_date, last_location, last_date):
     payload = {
         "currencyCode": "TWD",
         "originDestinations": [
+            # 4 flights
             {
                 "id": "1",
                 "originLocationCode": first_location,
                 "destinationLocationCode": "TPE",
                 "departureDateTimeRange": {"date": first_date},
             },
+            
+            # assume the middle 2 flights are fixed
             {
                 "id": "2",
                 "originLocationCode": "TPE",
@@ -44,6 +60,7 @@ def search(first_location, first_date, last_location, last_date):
                 "destinationLocationCode": "TPE",
                 "departureDateTimeRange": {"date": "2025-07-19"},
             },
+            
             {
                 "id": "4",
                 "originLocationCode": "TPE",
@@ -79,9 +96,7 @@ def search(first_location, first_date, last_location, last_date):
     data = res.json()
     return data
 
-
-# 長榮
-EVA_locations = ["CNX", "HKG", "FUK", "OKA", "DAD", "BKK", "PEN", "CTS", "HAN", "SDJ", "KMQ", "AMS", "SGN"]
+# Update the locations & dates you are interested 
 ALL_locations = ["CNX", "NGO", "HKG", "OKA", "DAD", "BKK", "PEN", "CTS", "HAN", "SDJ", "HKD", "SGN"]
 for start_loc in ALL_locations:
     for start_date in ["2025-04-05", "2025-04-06"]:
